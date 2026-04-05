9f8dac81d3b3972936343110772b10abdbc74407
SofiaShabatura <shabaturasofia0@gmail.com>
2026-04-05 17:07:36 +0300

    Update custom user with phone auth

--- users/models.py
+++ users/models.py
@@ -30,6 +30,7 @@ class CustomUserManager(BaseUserManager):
 
 
 class User(AbstractBaseUser, PermissionsMixin):
+    username = None 
     phone = models.CharField(max_length=20, unique=True)
 
     is_active = models.BooleanField(default=True)
