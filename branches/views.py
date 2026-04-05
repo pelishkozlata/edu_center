d7d2a1be7d15aa384d26e55f6d40a74cd9092895
SofiaShabatura <shabaturasofia0@gmail.com>
2026-04-05 16:42:03 +0300

    Add branches app

--- branches/views.py
+++ branches/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from .models import Branch
+
+
+def branch_list(request):
+    branches = Branch.objects.all()
+    return render(request, 'branches/branch_list.html', {'branches': branches})
