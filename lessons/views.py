b977b7e093f9b91b28553a580e693cb3a6eba6fe
pelishkozlata <pelishkozlata@gmail.com>
2026-04-06 20:08:46 +0300

    Updated views for lessons

--- lessons/views.py
+++ lessons/views.py
@@ -10,12 +10,25 @@ def lesson_list(request):
 def lesson_create(request):
     if request.method == 'POST':
         form = LessonForm(request.POST)
+
         if form.is_valid():
-            form.save()
-            return redirect('lesson_list')
+            lesson = form.save(commit=False)
+
+            if lesson.type == 'INDIVIDUAL' and not lesson.student:
+                form.add_error('student', 'Required for individual lesson')
+
+            elif lesson.type == 'GROUP' and not lesson.group:
+                form.add_error('group', 'Required for group lesson')
+
+            else:
+                lesson.save()
+                return redirect('lesson_list')
+
     else:
         form = LessonForm()
+
     return render(request, 'lessons/form.html', {'form': form})
+
 
 def lesson_detail(request, pk):
     lesson = get_object_or_404(Lesson, pk=pk)
