554e07b454ec4e4ca48585ae46c863ff5175b1aa
pelishkozlata <pelishkozlata@gmail.com>
2026-04-06 11:58:32 +0300

    Added views and templates to my apps

--- attendance/views.py
+++ attendance/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+# attendance/views.py
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Attendance
+from .forms import AttendanceForm
 
-# Create your views here.
+def attendance_list(request):
+    records = Attendance.objects.all()
+    return render(request, 'attendance/list.html', {'records': records})
+
+def attendance_mark(request):
+    if request.method == 'POST':
+        form = AttendanceForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('attendance_list')
+    else:
+        form = AttendanceForm()
+    return render(request, 'attendance/form.html', {'form': form})
