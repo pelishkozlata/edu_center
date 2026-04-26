e16695b1557492be0b5a803f82fe02b39fa9d327
pelishkozlata <pelishkozlata@gmail.com>
2026-04-26 21:38:10 +0300

    JWT authentication

--- edu_center/urls.py
+++ edu_center/urls.py
@@ -19,6 +19,8 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from groups.views import GroupViewSet, GroupStudentViewSet
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 
 router = DefaultRouter()
 router.register('groups', GroupViewSet)
@@ -26,7 +28,12 @@ router.register('group-students', GroupStudentViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    
     path('api/', include(router.urls)),
 ]
