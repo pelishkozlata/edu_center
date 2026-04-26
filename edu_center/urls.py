c50af6dbad0c16168b8196d2105614ed3993255c
pelishkozlata <pelishkozlata@gmail.com>
2026-04-26 21:17:38 +0300

    swagger + openAPI

--- edu_center/urls.py
+++ edu_center/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from groups.views import GroupViewSet, GroupStudentViewSet
-
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 router = DefaultRouter()
 router.register('groups', GroupViewSet)
@@ -26,5 +26,7 @@ router.register('group-students', GroupStudentViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/', include(router.urls)),
 ]
