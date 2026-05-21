0cc251a1ffd6f18738d551e4633a8c32c647592f
SofiaShabatura <shabaturasofia0@gmail.com>
2026-05-21 21:55:23 +0300

    students test

--- students/tests.py
+++ students/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
 
 # Create your tests here.
+from branches.models import Branch
+from students.models import Student
+
+class StudentBasicTest(TestCase):
+    def test_create_student_successfully(self):
+        branch = Branch.objects.create(name="Львів-Центр", city="Львів", status="active")
+
+        student = Student.objects.create(
+            first_name="Софія", 
+            last_name="Тест", 
+            branch=branch
+        )
+
+        self.assertEqual(Student.objects.count(), 1)
+        self.assertEqual(student.first_name, "Софія")
+        self.assertEqual(student.branch.name, "Львів-Центр")
